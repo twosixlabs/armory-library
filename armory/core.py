2b019eb41d4115cd69a19d70b389123fc986c565
Christopher Woodall <woodall.christopher@gmail.com>
2023-03-09 14:38:45 -0600

    copy os environ

--- armory/core.py
+++ armory/core.py
@@ -13,10 +13,9 @@ from armory.logs import added_filters, is_debug, log
 from armory.utils.printing import bold, red
 
 
-# TODO: Refactor -CW
 class ArmoryInstance:
     def __init__(self, envs: dict = None):
-        self.env = os.environ
+        self.env = os.environ.copy()
         for k, v in envs.items():
             self.env[str(k)] = str(v)
 
@@ -29,7 +28,7 @@ class ArmoryInstance:
         return completion.returncode
 
 
-class Evaluator(object):
+class Evaluator:
     """
     Evaluators control launching of ARMORY evaluations.
     """
@@ -58,11 +57,11 @@ class Evaluator(object):
         eval_id = f"{output_dir}_{date_time}" if output_dir else date_time
 
         self.manager = ArmoryInstance
+        self.no_docker = True
+        self.root = False
         self.config["eval_id"] = eval_id
         self.output_dir = os.path.join(self.host_paths.output_dir, eval_id)
         self.tmp_dir = os.path.join(self.host_paths.tmp_dir, eval_id)
-        self.no_docker = True
-        self.root = False
 
         # Retrieve environment variables that should be used in evaluation
         log.info("Retrieving Environment Variables")
@@ -76,9 +75,9 @@ class Evaluator(object):
             "VERIFY_SSL": self.armory_global_config["verify_ssl"] or False,
             "NVIDIA_VISIBLE_DEVICES": self.config["sysconfig"].get("gpus", None),
             "PYTHONHASHSEED": self.config["sysconfig"].get("set_pythonhashseed", "0"),
-            # "HOME": "/tmp",
             "TORCH_HOME": paths.HostPaths().pytorch_dir,
             environment.ARMORY_VERSION: armory.__version__,
+            # "HOME": "/tmp",
         }
 
     def run(
@@ -100,7 +99,7 @@ class Evaluator(object):
             base64_bytes = base64.b64encode(bytes_config)
             base64_config = base64_bytes.decode("utf-8")
 
-            options = self._build_options(
+            options = self._options_builder(
                 check_run=check_run,
                 num_eval_batches=num_eval_batches,
                 skip_benign=skip_benign,
@@ -119,29 +118,7 @@ class Evaluator(object):
         self._cleanup()
         return exit_code
 
-    def _cleanup(self):
-        log.info(f"deleting tmp_dir {self.tmp_dir}")
-        try:
-            shutil.rmtree(self.tmp_dir)
-        except OSError as e:
-            if not isinstance(e, FileNotFoundError):
-                log.exception(f"Error removing tmp_dir {self.tmp_dir}")
-
-        try:
-            os.rmdir(self.output_dir)
-            log.warning(f"removed output_dir {self.output_dir} because it was empty")
-        except FileNotFoundError:
-            log.warning(f"output_dir {self.output_dir} was deleted or never created")
-        except OSError:
-            jsons = [x for x in os.listdir(self.output_dir) if x.endswith(".json")]
-            if len(jsons) == 1:
-                json = jsons[0]
-            else:
-                json = ""
-            output_path = os.path.join(self.output_dir, json)
-            log.info(f"results output written to:\n{output_path}")
-
-    def _build_options(
+    def _options_builder(
         self,
         check_run,
         num_eval_batches,
@@ -150,8 +127,7 @@ class Evaluator(object):
         skip_misclassified,
         validate_config,
     ):
-        options = ""
-        options += " --no-docker"
+        options = "--no-docker"
 
         if check_run:
             options += " --check"
@@ -170,3 +146,22 @@ class Evaluator(object):
         for module, level in added_filters.items():
             options += f" --log-level {module}:{level}"
         return options
+
+    def _cleanup(self):
+        log.info(f"deleting tmp_dir {self.tmp_dir}")
+        try:
+            shutil.rmtree(self.tmp_dir)
+        except OSError as e:
+            if not isinstance(e, FileNotFoundError):
+                log.exception(f"Error removing tmp_dir {self.tmp_dir}")
+
+        try:
+            os.rmdir(self.output_dir)
+            log.warning(f"removed output_dir {self.output_dir} because it was empty")
+        except FileNotFoundError:
+            log.warning(f"output_dir {self.output_dir} was deleted or never created")
+        except OSError:
+            jsons = [x for x in os.listdir(self.output_dir) if x.endswith(".json")]
+            json_output = jsons[0] if len(jsons) == 1 else ""
+            output_path = os.path.join(self.output_dir, json_output)
+            log.info(f"results output written to:\n{output_path}")
