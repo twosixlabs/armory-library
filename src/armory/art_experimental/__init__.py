daa533e184e2c6c55540642c33b057a99ee92f69
Christopher Woodall <woodall.christopher@gmail.com>
2023-03-15 10:56:07 -0500

    minimize the amount of delayed imports

--- src/armory/art_experimental/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This subpackage will contain experimental ART features that are not merged into
-the PyPi package.
-"""
-from armory import delayed_imports
--- src/armory/baseline_models/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Baseline models for use in the ARMORY system.
-"""
-from armory import delayed_imports
