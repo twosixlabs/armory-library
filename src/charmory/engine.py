078bb92cca8f6776e552fccda40525ae607e7c34
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-07-19 14:02:27 -0400

    Replace train kwargs with explicit args

--- src/charmory/engine.py
+++ src/charmory/engine.py
@@ -3,13 +3,18 @@ class Engine:
         self.evaluation = evaluation
         self.scenario = evaluation.scenario.function(self.evaluation)
 
-    def train(self, **fit_kwargs):
-        """Train the model using the configured training dataset"""
+    def train(self, nb_epochs=1):
+        """
+        Train the evaluation model using the configured training dataset.
+
+        Args:
+            nb_epochs: Number of epochs with which to perform training
+        """
         assert self.evaluation.dataset.train_dataset is not None, (
             "Requested to train the model but the evaluation dataset does not "
             "provide a train_dataset"
         )
-        self.scenario.fit(**fit_kwargs)
+        self.scenario.fit(nb_epochs=nb_epochs)
 
     def run(self):
         results = self.scenario.evaluate()
