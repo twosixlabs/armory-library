455d737dfda743ab07ddbac31dfc1a7a8aa0f77e
Christopher Woodall <christopher.woodall@twosixtech.com>
2023-08-01 14:02:22 -0500

    remove dead code

--- src/armory/utils/configuration.py
+++ src/armory/utils/configuration.py
@@ -6,10 +6,6 @@ import json
 import os
 from pathlib import Path
 
-import jsonschema
-
-DEFAULT_SCHEMA = os.path.join(os.path.dirname(__file__), "config_schema.json")
-
 
 def get_configured_path(env_var: str, default_subdir: str) -> str:
     # Retrieve the value of the environment variable
@@ -29,18 +25,6 @@ def get_verify_ssl():
     return os.getenv("VERIFY_SSL") == "true" or os.getenv("VERIFY_SSL") is None
 
 
-def validate_config(config: dict, schema_path: str = DEFAULT_SCHEMA) -> dict:
-    """
-    Validates that a config matches the default JSON Schema
-    """
-    with open(schema_path, "r") as schema_file:
-        schema = json.load(schema_file)
-
-    jsonschema.validate(instance=config, schema=schema)
-
-    return config
-
-
 def load_config(filepath: str) -> dict:
     """
     Loads and validates a config file
@@ -48,4 +32,4 @@ def load_config(filepath: str) -> dict:
     with open(filepath) as f:
         config = json.load(f)
 
-    return validate_config(config)
+    return config
