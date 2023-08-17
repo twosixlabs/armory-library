c5b1e3883f018e6fec29e4f0485b928ea8882437
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-17 12:28:19 -0400

    Add prototype of accuracy metric

--- src/charmory/tasks/image_classification.py
+++ src/charmory/tasks/image_classification.py
@@ -1,7 +1,25 @@
 """Image classification evaluation task"""
 
+from pprint import pprint
+
+import torch
+import torchmetrics.classification
+
 from charmory.tasks.base import BaseEvaluationTask
 
 
 class ImageClassificationTask(BaseEvaluationTask):
     """Image classification evaluation task"""
+
+    def __init__(self, *args, num_classes: int, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.accuracy = torchmetrics.classification.Accuracy(
+            task="multiclass", num_classes=num_classes
+        )
+
+    def run_benign(self, batch: BaseEvaluationTask.Batch):
+        super().run_benign(batch)
+        self.accuracy(torch.tensor(batch.y_pred), torch.tensor(batch.y))
+
+    def on_test_end(self):
+        pprint(self.accuracy.compute())
