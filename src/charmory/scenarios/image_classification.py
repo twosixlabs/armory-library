f511ff3382f0332f49f32d4ffcb7709abadeaafc
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-22 16:42:56 -0400

    Remove first attempt at lightning-compatible scenario

--- src/charmory/scenarios/image_classification.py
+++ src/charmory/scenarios/image_classification.py
@@ -4,14 +4,8 @@ General image classification scenario
 
 from armory.instrument.export import ImageClassificationExporter
 from charmory.scenario import Scenario
-from charmory.scenarios.base import BaseScenario
 
 
 class ImageClassificationTask(Scenario):
     def _load_sample_exporter(self):
         return ImageClassificationExporter(self.export_dir)
-
-
-class ImageClassificationModule(BaseScenario):
-    def _load_sample_exporter(self, export_dir):
-        return ImageClassificationExporter(export_dir)
