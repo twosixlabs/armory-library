f206f64c71d5ab461a522c25ec51ffb1d10e2171
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-22 11:48:38 -0400

    Implement mean average precision metrics for OD task

--- src/charmory/tasks/object_detection.py
+++ src/charmory/tasks/object_detection.py
@@ -1,12 +1,77 @@
 """Object detection evaluation task"""
 
-# import torch
-# import torchmetrics.classification
+from pprint import pprint
+
+import torch
+import torchmetrics.detection
 
 from armory.instrument.export import ObjectDetectionExporter
 
 # from charmory.metrics.perturbation import PerturbationNormMetric
 from charmory.tasks.base import BaseEvaluationTask
+
+
+class MAP(torchmetrics.detection.MeanAveragePrecision):
+    """Extension of torchmetrics MeanAveragePrecision to make it work with Armory & Lightning"""
+
+    def __init__(self, prefix: str, **kwargs):
+        super().__init__(**kwargs)
+        self.prefix = prefix
+
+    def update(self, preds, target):
+        """
+        Convert predictions and target from Armory/ART types to torchmetrics-compatible types
+        """
+        super().update(self._create_map_preds(preds), self._create_map_target(target))
+
+    @staticmethod
+    def _create_map_preds(preds):
+        return [
+            {
+                "boxes": torch.FloatTensor(pred["boxes"]),
+                "scores": torch.FloatTensor(pred["scores"]),
+                "labels": torch.IntTensor(pred["labels"]),
+            }
+            for pred in preds
+        ]
+
+    @staticmethod
+    def _create_map_target(targets):
+        return [
+            {
+                "boxes": torch.FloatTensor(target["boxes"]),
+                "labels": torch.IntTensor(target["labels"]),
+                "area": torch.FloatTensor(target["area"]),
+            }
+            for target in targets
+        ]
+
+    def compute(self) -> dict:
+        """
+        Convert torchmetrics results to Lightning-compatible metrics
+        """
+        metrics = {f"{self.prefix}_{k}": v for k, v in super().compute().items()}
+        pprint(metrics)
+        metrics.pop(f"{self.prefix}_classes")
+        maps_per_class = metrics.pop(f"{self.prefix}_map_per_class")
+        if self.class_metrics and maps_per_class is not None:
+            metrics.update(
+                {
+                    f"{self.prefix}_map_class_{id}": value
+                    for id, value in enumerate(maps_per_class)
+                    if value != -1
+                }
+            )
+        mars_per_class = metrics.pop(f"{self.prefix}_mar_100_per_class")
+        if self.class_metrics and mars_per_class is not None:
+            metrics.update(
+                {
+                    f"{self.prefix}_mar_class_{id}": value
+                    for id, value in enumerate(mars_per_class)
+                    if value != -1
+                }
+            )
+        return metrics
 
 
 class ObjectDetectionTask(BaseEvaluationTask):
@@ -15,23 +80,15 @@ class ObjectDetectionTask(BaseEvaluationTask):
     def __init__(
         self,
         *args,
-        # num_classes: int,
         # perturbation_ord: float = torch.inf,
+        class_metrics: bool = False,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        # self.benign_accuracy = torchmetrics.classification.Accuracy(
-        #     task="multiclass", num_classes=num_classes
-        # )
-        # self.attack_accuracy = torchmetrics.classification.Accuracy(
-        #     task="multiclass", num_classes=num_classes
-        # )
+        self.benign_map = MAP(prefix="benign", class_metrics=class_metrics)
+        self.attack_map = MAP(prefix="attack", class_metrics=class_metrics)
         # self.perturbation = PerturbationNormMetric(ord=perturbation_ord)
         self.sample_exporter = ObjectDetectionExporter(self.export_dir)
-
-    ###
-    # Export methods
-    ###
 
     def export_batch(self, batch: BaseEvaluationTask.Batch):
         self._export("benign", batch.x, batch.y, batch.y_pred, batch.i)
@@ -52,13 +109,22 @@ class ObjectDetectionTask(BaseEvaluationTask):
 
     def run_benign(self, batch: BaseEvaluationTask.Batch):
         super().run_benign(batch)
-        # self.benign_accuracy(torch.tensor(batch.y_pred), torch.tensor(batch.y))
-        # self.log("benign_accuracy", self.benign_accuracy)
+        if batch.y_pred is not None:
+            self.benign_map.update(batch.y_pred, batch.y)
 
     def run_attack(self, batch: BaseEvaluationTask.Batch):
         super().run_attack(batch)
-        # self.attack_accuracy(torch.tensor(batch.y_pred_adv), torch.tensor(batch.y))
-        # self.log("attack_accuracy", self.attack_accuracy)
+        if batch.y_pred_adv is not None:
+            self.attack_map.update(batch.y_pred_adv, batch.y)
 
         # self.perturbation(torch.tensor(batch.x), torch.tensor(batch.x_adv))
         # self.log("perturbation", self.perturbation)
+
+    def on_test_epoch_end(self):
+        if not self.skip_benign:
+            self.log_dict(self.benign_map.compute(), sync_dist=True)
+            self.benign_map.reset()
+        if not self.skip_attack:
+            self.log_dict(self.attack_map.compute(), sync_dist=True)
+            self.attack_map.reset()
+        return super().on_validation_epoch_end()
