2acb0ddb08564a52064840e311d8c520e3b0bdca
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-23 14:40:28 -0400

    Attempt to save off tracked params with return objects, doesn't work for nested objects

--- src/charmory/track.py
+++ src/charmory/track.py
@@ -4,7 +4,17 @@ from functools import wraps
 import os
 from pathlib import Path
 import sys
-from typing import Callable, Mapping, Optional, Sequence, TypeVar, Union, overload
+from typing import (
+    Any,
+    Callable,
+    Mapping,
+    Optional,
+    Sequence,
+    Set,
+    TypeVar,
+    Union,
+    overload,
+)
 
 import mlflow
 import mlflow.cli
@@ -16,6 +26,25 @@ from typing_extensions import ParamSpec
 
 P = ParamSpec("P")
 T = TypeVar("T")
+TRACKED_PARAMS_ATTR = "__tracked_params__"
+
+
+def _has_tracked_params(target: object) -> bool:
+    """
+    Checks if the given target object has tracked parameters. That is, it was created by a
+    function that was wrapped with `track_params` or a class wrapped with `track_init_params`
+    """
+    return hasattr(target, TRACKED_PARAMS_ATTR)
+
+
+def _get_tracked_params(target: object) -> Mapping[str, Any]:
+    """Get tracked parameters from the given target object."""
+    return getattr(target, TRACKED_PARAMS_ATTR)
+
+
+def _set_tracked_params(target: object, params: Mapping[str, Any]):
+    """Set tracked parameters for the given target object"""
+    setattr(target, TRACKED_PARAMS_ATTR, params)
 
 
 @overload
@@ -23,6 +52,7 @@ def track_params(
     *,
     prefix: Optional[str] = None,
     ignore: Optional[Sequence[str]] = None,
+    self_is_target: bool = False,
 ) -> Callable[[Callable[P, T]], Callable[P, T]]:
     ...
 
@@ -33,6 +63,7 @@ def track_params(
     *,
     prefix: Optional[str] = None,
     ignore: Optional[Sequence[str]] = None,
+    self_is_target: bool = False,
 ) -> Callable[P, T]:
     ...
 
@@ -42,6 +73,7 @@ def track_params(
     *,
     prefix: Optional[str] = None,
     ignore: Optional[Sequence[str]] = None,
+    self_is_target: bool = False,
 ):
     """
     Create a decorator to log function keyword arguments as parameters with
@@ -64,6 +96,9 @@ def track_params(
         prefix: Optional prefix for all keyword argument names (default is
             inferred from decorated function name)
         ignore: Optional list of keyword arguments to be ignored
+        self_is_target: First argument to the decorated function (i.e., self)
+            is the target to which the tracked parameters should be applied
+            (default is the return from the decorated function)
 
     Returns:
         Decorated function if `_func` was provided, else a function decorator
@@ -72,31 +107,20 @@ def track_params(
     def _decorator(func: Callable[P, T]) -> Callable[P, T]:
         @wraps(func)
         def _wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            active_run = mlflow.active_run()
-            if active_run:
-                _prefix = prefix if prefix else func.__name__
+            _prefix = prefix if prefix else func.__name__
 
-                # MLFlow does not allow duplicates, so check the active
-                # run and adjust the prefix if needed
-                count = 0
-                param = _prefix
-                while param in active_run.data.params:
-                    count += 1
-                    param = f"{_prefix}.{count}"
-                if count:
-                    _prefix = f"{_prefix}.{count}"
-                active_run.data.params[_prefix] = True
+            params: Mapping[str, Any] = {
+                f"{_prefix}._func": f"{func.__module__}.{func.__qualname__}"
+            }
 
-                mlflow.log_param(
-                    f"{_prefix}._func", f"{func.__module__}.{func.__qualname__}"
-                )
+            for key, val in kwargs.items():
+                if ignore and key in ignore:
+                    continue
+                params[f"{_prefix}.{key}"] = val
 
-                for key, val in kwargs.items():
-                    if ignore and key in ignore:
-                        continue
-                    mlflow.log_param(f"{_prefix}.{key}", val)
-
-            return func(*args, **kwargs)
+            result = func(*args, **kwargs)
+            _set_tracked_params(args[0] if self_is_target else result, params)
+            return result
 
         return _wrapper
 
@@ -160,13 +184,87 @@ def track_init_params(
 
     def _decorator(cls: T) -> T:
         _prefix = prefix if prefix else getattr(cls, "__name__", "")
-        cls.__init__ = track_params(prefix=_prefix, ignore=ignore)(cls.__init__)
+        cls.__init__ = track_params(prefix=_prefix, ignore=ignore, self_is_target=True)(
+            cls.__init__
+        )
         return cls
 
     if _cls is None:
         return _decorator
     else:
         return _decorator(_cls)
+
+
+def get_tracked_params(target: object, recursive: bool = True) -> Mapping[str, Any]:
+    params = {}
+    if _has_tracked_params(target):
+        params.update(_get_tracked_params(target))
+    # if recursive:
+    #     for attr in dir(target):
+    #         child_params = get_tracked_params(getattr(target, attr, {}))
+    #         count = 0
+    #         for key, val in child_params.items():
+    #             if count:
+    #                 key = f"{key}.{count}"
+
+    # # MLFlow does not allow duplicates, so check the active
+    # # run and adjust the prefix if needed
+    # count = 0
+    # param = _prefix
+    # while param in active_run.data.params:
+    #     count += 1
+    #     param = f"{_prefix}.{count}"
+    # if count:
+    #     _prefix = f"{_prefix}.{count}"
+    # active_run.data.params[_prefix] = True
+
+    return params
+
+
+def track_params_from(
+    target: object,
+    prior_keys: Optional[Set[str]] = None,
+    prior_targets: Optional[Set[object]] = None,
+    max_recursion_depth: int = 4,
+):
+    if prior_keys is None:
+        prior_keys = set()
+    if prior_targets is None:
+        prior_targets = set()
+
+    if target in prior_targets:
+        return
+    prior_targets.add(target)
+
+    print(f"checking for params from {target}")
+    if _has_tracked_params(target):
+        print(f"logging params from {target}")
+        params = _get_tracked_params(target)
+        # MLFlow does not allow duplicate parameters, so check against prior
+        # logged keys and adjust the parameter keys with a count if needed
+        count = 0
+        for key, val in params.items():
+            print(f"  has key {key}")
+            if count:
+                key = f"{key}.{count}"
+            else:
+                tmp = key
+                while tmp in prior_keys:
+                    count += 1
+                    tmp = f"{key}.{count}"
+                key = tmp
+            print(f"  using key {key}")
+            mlflow.log_param(key, val)
+            prior_keys.add(key)
+
+    if max_recursion_depth > 0:
+        for attr in dir(target):
+            child = getattr(target, attr, None)
+            if child is not None:
+                print(f"checking child {attr}")
+                track_params_from(
+                    child, prior_keys, prior_targets, max_recursion_depth - 1
+                )
 
 
 def track_evaluation(
