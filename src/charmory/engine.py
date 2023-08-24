afdf325624835a3f6ee517249b3c58398f1c391d
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-24 12:37:14 -0400

    Add tracking methods to Engine to support logging to a finished run

--- src/charmory/engine.py
+++ src/charmory/engine.py
@@ -1,3 +1,7 @@
+from typing import Any, Dict, Optional
+
+import mlflow
+
 from armory.logs import log
 from charmory.evaluation import Evaluation
 from charmory.track import track_evaluation, track_metrics
@@ -7,7 +11,7 @@ class Engine:
     def __init__(self, evaluation: Evaluation):
         self.evaluation = evaluation
         self.scenario = evaluation.scenario.function(self.evaluation)
-        self._has_run = False
+        self._run_id: Optional[str] = None
 
     def train(self, nb_epochs=1):
         """
@@ -31,17 +35,37 @@ class Engine:
         )
 
     def run(self):
-        if self._has_run:
+        if self._run_id:
             raise RuntimeError(
                 "Evaluation engine has already been run. Create a new Engine "
                 "instance to perform a subsequent run."
             )
-        self._has_run = True
 
         with track_evaluation(
             name=self.evaluation.name, description=self.evaluation.description
-        ):
+        ) as active_run:
+            self._run_id = active_run.info.run_id
             results = self.scenario.evaluate()
             track_metrics(results["results"]["metrics"])
 
             return results
+
+    def log_params(self, params: Dict[str, Any]):
+        if not self._run_id:
+            raise RuntimeError("Engine must be run before `log_params` may be called.")
+        with mlflow.start_run(run_id=self._run_id):
+            mlflow.log_params(params)
+
+    def log_metrics(self, metrics: Dict[str, float], step: Optional[int] = None):
+        if not self._run_id:
+            raise RuntimeError("Engine must be run before `log_metric` may be called.")
+        with mlflow.start_run(run_id=self._run_id):
+            mlflow.log_metrics(metrics, step)
+
+    def log_artifacts(self, local_dir: str, artifact_path: Optional[str] = None):
+        if not self._run_id:
+            raise RuntimeError(
+                "Engine must be run before `log_artifacts` may be called."
+            )
+        with mlflow.start_run(run_id=self._run_id):
+            mlflow.log_artifacts(local_dir, artifact_path)
