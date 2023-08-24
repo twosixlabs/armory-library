a42627507abed854eb4d57bf2543c719ce85cb62
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-24 14:03:44 -0400

    Update tracking unit tests

--- tests/unit/test_track.py
+++ tests/unit/test_track.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import tempfile
-from unittest.mock import Mock, call
+from unittest.mock import Mock, call, patch
 
 import mlflow
 import pytest
@@ -20,17 +20,19 @@ pytestmark = pytest.mark.unit
 
 
 @pytest.fixture(autouse=True)
+def reset_params():
+    yield
+    track.reset_params()
+
+
+@pytest.fixture(autouse=True)
 def temp_mlflow_uri():
     with tempfile.TemporaryDirectory() as tmpdir:
         orig_tracking_uri = mlflow.get_tracking_uri()
         mlflow.set_tracking_uri(Path(tmpdir))
-        yield
+        with patch.dict("os.environ", {"MLFLOW_TRACKING_URI": Path(tmpdir).as_uri()}):
+            yield
         mlflow.set_tracking_uri(orig_tracking_uri)
-
-
-@pytest.fixture
-def experiment_id():
-    return mlflow.create_experiment("test")
 
 
 ###
@@ -39,107 +41,99 @@ def experiment_id():
 
 
 ###
+# tracking_context
+###
+
+
+def test_tracking_context_isolates_parameters():
+    track.track_param("key", "global")
+
+    with track.tracking_context():
+        track.track_param("key", "child")
+        assert track._get_current_params() == {"key": "child"}
+
+    assert track._get_current_params() == {"key": "global"}
+
+
+def test_tracking_context_when_nested():
+    track.track_param("key1", "global")
+
+    with track.tracking_context(nested=True):
+        track.track_param("key2", "child")
+        assert track._get_current_params() == {"key1": "global", "key2": "child"}
+
+
+###
 # track_params
 ###
 
 
-def test_track_params_when_inline(experiment_id):
+def test_track_params_when_inline():
     def func(a, b):
         assert a == "hello world"
         assert b == 42
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        track.track_params(func)(a="hello world", b=42)
+    track.track_params(func)(a="hello world", b=42)
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "func._func" in run.data.params
-    assert run.data.params["func.a"] == "hello world"
-    assert run.data.params["func.b"] == "42"
+    params = track._get_current_params()
+    params.pop("func._func")
+    assert params == {"func.a": "hello world", "func.b": 42}
 
 
-def test_track_params_when_decorator(experiment_id):
+def test_track_params_when_decorator():
     @track.track_params
     def funct(a, b):
         assert a == 314159
         assert b == "hi"
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        funct(a=314159, b="hi")
+    funct(a=314159, b="hi")
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "funct._func" in run.data.params
-    assert run.data.params["funct.a"] == "314159"
-    assert run.data.params["funct.b"] == "hi"
+    params = track._get_current_params()
+    params.pop("funct._func")
+    assert params == {"funct.a": 314159, "funct.b": "hi"}
 
 
-def test_track_params_with_prefix(experiment_id):
+def test_track_params_with_prefix():
     @track.track_params(prefix="the_func")
     def func(a, b):
         assert a == "jenny"
         assert b == 8675309
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        func(a="jenny", b=8675309)
+    func(a="jenny", b=8675309)
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "the_func._func" in run.data.params
-    assert run.data.params["the_func.a"] == "jenny"
-    assert run.data.params["the_func.b"] == "8675309"
+    params = track._get_current_params()
+    params.pop("the_func._func")
+    assert params == {"the_func.a": "jenny", "the_func.b": 8675309}
 
 
-def test_track_params_with_ignore(experiment_id):
+def test_track_params_with_ignore():
     def func(a, b, c):
         assert a == "arg"
         assert b == 7
         assert c == "kwarg"
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        track.track_params(func, ignore=["c"])("arg", b=7, c="kwarg")
+    track.track_params(func, ignore=["c"])("arg", b=7, c="kwarg")
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "func._func" in run.data.params
-    assert "func.a" not in run.data.params
-    assert run.data.params["func.b"] == "7"
-    assert "func.c" not in run.data.params
+    params = track._get_current_params()
+    params.pop("func._func")
+    assert params == {"func.b": 7}
 
 
-def test_track_params_when_no_active_run():
-    func = Mock()
-
-    track.track_params(func)(a="hello", b="world")
-
-    func.assert_called_once_with(a="hello", b="world")
-    try:
-        assert not mlflow.last_active_run()
-    except mlflow.exceptions.MlflowException:
-        # Exception is raised when it has a last-run ID but the run doesn't exist
-        # (because we delete the directory after each test)
-        pass
-
-
-def test_track_params_when_multiple_calls(experiment_id):
+def test_track_params_when_multiple_calls():
     mock = Mock()
 
     @track.track_params
     def func(**kwargs):
         mock(**kwargs)
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        func(name="John Doe")
-        func(name="Jane Doe")
+    func(name="John Doe")
+    func(name="Jane Doe")
 
     mock.assert_has_calls([call(name="John Doe"), call(name="Jane Doe")])
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "func._func" in run.data.params
-    assert run.data.params["func.name"] == "John Doe"
-    assert "func.1._func" in run.data.params
-    assert run.data.params["func.1.name"] == "Jane Doe"
+    params = track._get_current_params()
+    params.pop("func._func")
+    assert params == {"func.name": "Jane Doe"}
 
 
 ###
@@ -147,93 +141,62 @@ def test_track_params_when_multiple_calls(experiment_id):
 ###
 
 
-def test_track_init_params_when_inline(experiment_id):
+def test_track_init_params_when_inline():
     class TestClass:
         def __init__(self, a, b):
             assert a == "hello world"
             assert b == 42
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        track.track_init_params(TestClass)(a="hello world", b=42)
+    track.track_init_params(TestClass)(a="hello world", b=42)
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "TestClass._func" in run.data.params
-    assert run.data.params["TestClass.a"] == "hello world"
-    assert run.data.params["TestClass.b"] == "42"
+    params = track._get_current_params()
+    params.pop("TestClass._func")
+    assert params == {"TestClass.a": "hello world", "TestClass.b": 42}
 
 
-def test_track_init_params_when_decorator(experiment_id):
+def test_track_init_params_when_decorator():
     @track.track_init_params
     class ClassType:
         def __init__(self, a, b):
             assert a == 314159
             assert b == "hi"
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        ClassType(a=314159, b="hi")
+    ClassType(a=314159, b="hi")
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "ClassType._func" in run.data.params
-    assert run.data.params["ClassType.a"] == "314159"
-    assert run.data.params["ClassType.b"] == "hi"
+    params = track._get_current_params()
+    params.pop("ClassType._func")
+    assert params == {"ClassType.a": 314159, "ClassType.b": "hi"}
 
 
-def test_track_init_params_with_prefix(experiment_id):
+def test_track_init_params_with_prefix():
     @track.track_init_params(prefix="the_class")
     class TestClass:
         def __init__(self, a, b):
             assert a == "jenny"
             assert b == 8675309
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        TestClass(a="jenny", b=8675309)
+    TestClass(a="jenny", b=8675309)
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "the_class._func" in run.data.params
-    assert run.data.params["the_class.a"] == "jenny"
-    assert run.data.params["the_class.b"] == "8675309"
+    params = track._get_current_params()
+    params.pop("the_class._func")
+    assert params == {"the_class.a": "jenny", "the_class.b": 8675309}
 
 
-def test_track_init_params_with_ignore(experiment_id):
+def test_track_init_params_with_ignore():
     class TestClass:
         def __init__(self, a, b, c):
             assert a == "arg"
             assert b == 7
             assert c == "kwarg"
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        track.track_init_params(TestClass, ignore=["c"])("arg", b=7, c="kwarg")
+    track.track_init_params(TestClass, ignore=["c"])("arg", b=7, c="kwarg")
 
-    run = mlflow.last_active_run()
-    assert run
-    assert "TestClass._func" in run.data.params
-    assert "TestClass.a" not in run.data.params
-    assert run.data.params["TestClass.b"] == "7"
-    assert "TestClass.c" not in run.data.params
+    params = track._get_current_params()
+    params.pop("TestClass._func")
+    assert params == {"TestClass.b": 7}
 
 
-def test_track_init_params_when_no_active_run():
-    mock = Mock()
-
-    class TestClass:
-        def __init__(self, a, b):
-            mock(a, b)
-
-    track.track_init_params(TestClass)(a="hello", b="world")
-
-    mock.assert_called_once_with("hello", "world")
-    try:
-        assert not mlflow.last_active_run()
-    except mlflow.exceptions.MlflowException:
-        # Exception is raised when it has a last-run ID but the run doesn't exist
-        # (because we delete the directory after each test)
-        pass
-
-
-def test_track_init_params_when_multiple_calls(experiment_id):
+def test_track_init_params_when_multiple_calls():
     mock = Mock()
 
     @track.track_init_params
@@ -241,15 +204,28 @@ def test_track_init_params_when_multiple_calls(experiment_id):
         def __init__(self, **kwargs):
             mock(**kwargs)
 
-    with mlflow.start_run(experiment_id=experiment_id):
-        TestClass(name="John Doe")
-        TestClass(name="Jane Doe")
+    TestClass(name="John Doe")
+    TestClass(name="Jane Doe")
 
     mock.assert_has_calls([call(name="John Doe"), call(name="Jane Doe")])
 
+    params = track._get_current_params()
+    params.pop("TestClass._func")
+    assert params == {"TestClass.name": "Jane Doe"}
+
+
+###
+# track_evaluation
+###
+
+
+def test_track_evaluation_logs_params_with_mlflow():
+    track.track_param("strparam", "str")
+    track.track_param("intparam", 42)
+    with track.track_evaluation("test"):
+        pass
+
     run = mlflow.last_active_run()
     assert run
-    assert "TestClass._func" in run.data.params
-    assert run.data.params["TestClass.name"] == "John Doe"
-    assert "TestClass.1._func" in run.data.params
-    assert run.data.params["TestClass.1.name"] == "Jane Doe"
+    assert run.data.params["strparam"] == "str"
+    assert run.data.params["intparam"] == "42"
