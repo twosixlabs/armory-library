e65d8ecd7166e2ec7089a1674f44fb9469234d0f
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-29 10:16:29 -0400

    Update lightning engine to manage mlflow session

--- src/charmory/engine.py
+++ src/charmory/engine.py
@@ -59,13 +59,24 @@ class LightningEngine:
         limit_test_batches: Optional[Union[int, float]] = None,
     ):
         self.task = task
+        self.active_run = track_evaluation(
+            name=self.task.evaluation.name, description=self.task.evaluation.description
+        )
         self.trainer = pl.Trainer(
             inference_mode=False,
             limit_test_batches=limit_test_batches,
             logger=lightning_logger(),
         )
+        self.run_id: Optional[str] = None
 
     def run(self):
+        if self.run_id:
+            raise RuntimeError(
+                "Evaluation engine has already been run. Create a new LightningEngine "
+                "instance to perform a subsequent run."
+            )
+
+        self.run_id = self.active_run.info.run_id
         self.trainer.test(
             self.task, dataloaders=self.task.evaluation.dataset.test_dataset
         )
