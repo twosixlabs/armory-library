3e0a23a927587d5a9273947167cf12d333217159
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-08-29 10:16:29 -0400

    Update lightning engine to manage mlflow session

--- examples/src/charmory_examples/jatic_object_detection_lightning_example.py
+++ examples/src/charmory_examples/jatic_object_detection_lightning_example.py
@@ -9,114 +9,108 @@ from charmory.data import JaticObjectDetectionDatasetGenerator
 from charmory.engine import LightningEngine
 from charmory.evaluation import Attack, Dataset, Evaluation, Metric, Model, SysConfig
 from charmory.tasks.object_detection import ObjectDetectionTask
-from charmory.track import track_evaluation, track_init_params, track_params
+from charmory.track import track_init_params, track_params
 from charmory.utils import (
     adapt_jatic_object_detection_model_for_art,
     create_jatic_image_classification_dataset_transform,
 )
 
-NAME = "coco-object-detection"
-DESCRIPTION = "COCO object detection from HuggingFace & TorchVision"
-
 
 def main():
-    with track_evaluation(NAME, description=DESCRIPTION):
-        ###
-        # Model
-        ###
-        model = track_params(jatic_toolbox.load_model)(
-            provider="torchvision",
-            model_name="fasterrcnn_resnet50_fpn",
-            task="object-detection",
-        )
-        adapt_jatic_object_detection_model_for_art(model)
+    ###
+    # Model
+    ###
+    model = track_params(jatic_toolbox.load_model)(
+        provider="torchvision",
+        model_name="fasterrcnn_resnet50_fpn",
+        task="object-detection",
+    )
+    adapt_jatic_object_detection_model_for_art(model)
 
-        detector = track_init_params(PyTorchFasterRCNN)(
-            model,
-            channels_first=True,
-            clip_values=(0.0, 1.0),
-        )
+    detector = track_init_params(PyTorchFasterRCNN)(
+        model,
+        channels_first=True,
+        clip_values=(0.0, 1.0),
+    )
 
-        ###
-        # Dataset
-        ###
-        dataset = track_params(jatic_toolbox.load_dataset)(
-            provider="huggingface",
-            dataset_name="detection-datasets/coco",
-            task="object-detection",
-            split="train",
-        )
+    ###
+    # Dataset
+    ###
+    dataset = track_params(jatic_toolbox.load_dataset)(
+        provider="huggingface",
+        dataset_name="detection-datasets/coco",
+        task="object-detection",
+        split="train",
+    )
 
-        transform = create_jatic_image_classification_dataset_transform(
-            model.preprocessor
-        )
-        dataset.set_transform(transform)
+    transform = create_jatic_image_classification_dataset_transform(model.preprocessor)
+    dataset.set_transform(transform)
 
-        generator = JaticObjectDetectionDatasetGenerator(
-            dataset=dataset,
-            batch_size=1,  # have to use a batch size of 1 because of inhomogenous image sizes
-            epochs=1,
-        )
+    generator = JaticObjectDetectionDatasetGenerator(
+        dataset=dataset,
+        batch_size=1,  # have to use a batch size of 1 because of inhomogenous image sizes
+        epochs=1,
+    )
 
-        ###
-        # Evaluation
-        ###
-        eval_dataset = Dataset(
-            name="coco",
-            test_dataset=generator,
-        )
+    ###
+    # Evaluation
+    ###
+    eval_dataset = Dataset(
+        name="coco",
+        test_dataset=generator,
+    )
 
-        eval_model = Model(
-            name="faster-rcnn-resnet50",
-            model=detector,
-        )
+    eval_model = Model(
+        name="faster-rcnn-resnet50",
+        model=detector,
+    )
 
-        eval_attack = Attack(
-            name="PGD",
-            attack=track_init_params(art.attacks.evasion.RobustDPatch)(
-                detector,
-                patch_shape=(3, 40, 40),
-                batch_size=1,
-                max_iter=20,
-                targeted=False,
-                verbose=False,
-            ),
-            use_label_for_untargeted=False,
-        )
+    eval_attack = Attack(
+        name="PGD",
+        attack=track_init_params(art.attacks.evasion.RobustDPatch)(
+            detector,
+            patch_shape=(3, 40, 40),
+            batch_size=1,
+            max_iter=20,
+            targeted=False,
+            verbose=False,
+        ),
+        use_label_for_untargeted=False,
+    )
 
-        eval_metric = Metric(
-            profiler=BasicProfiler(),
-        )
+    eval_metric = Metric(
+        profiler=BasicProfiler(),
+    )
 
-        eval_sysconfig = SysConfig(
-            gpus=["all"],
-            use_gpu=True,
-        )
+    eval_sysconfig = SysConfig(
+        gpus=["all"],
+        use_gpu=True,
+    )
 
-        evaluation = Evaluation(
-            name=NAME,
-            description=DESCRIPTION,
-            author="",
-            dataset=eval_dataset,
-            model=eval_model,
-            attack=eval_attack,
-            scenario=None,
-            metric=eval_metric,
-            sysconfig=eval_sysconfig,
-        )
+    evaluation = Evaluation(
+        name="coco-object-detection",
+        description="COCO object detection from HuggingFace & TorchVision",
+        author="",
+        dataset=eval_dataset,
+        model=eval_model,
+        attack=eval_attack,
+        scenario=None,
+        metric=eval_metric,
+        sysconfig=eval_sysconfig,
+    )
 
-        ###
-        # Engine
-        ###
+    ###
+    # Engine
+    ###
 
-        task = ObjectDetectionTask(
-            evaluation,
-            skip_attack=True,
-            export_every_n_batches=5,
-            class_metrics=False,
-        )
-        engine = LightningEngine(task, limit_test_batches=80)
-        results = engine.run()
+    task = ObjectDetectionTask(
+        evaluation,
+        skip_attack=True,
+        export_every_n_batches=5,
+        class_metrics=False,
+    )
+    engine = LightningEngine(task, limit_test_batches=80)
+    results = engine.run()
 
     pprint(results)
 
