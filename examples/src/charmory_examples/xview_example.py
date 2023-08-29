9f09e7ee32c339171598188c7ad4d29d82f01807
Honaker <christopher.honaker@twosixtech.com>
2023-08-29 09:26:23 -0400

    xview file

---
+++ examples/src/charmory_examples/xview_example.py
@@ -0,0 +1,45 @@
+"""
+Example programmatic entrypoint for scenario execution
+"""
+import sys
+
+from jatic_toolbox import __version__ as jatic_version
+from jatic_toolbox import load_dataset as load_jatic_dataset
+
+import armory.baseline_models.pytorch.pokemon
+import armory.version
+
+dd
+
+def load_huggingface_dataset():
+    
+
+    train_dataset = load_jatic_dataset(
+        provider="huggingface",
+        dataset_name="Honaker/xview_dataset",
+        task="object-detection",
+        split="train",
+       
+    )
+
+ 
+
+    return train_dataset
+
+def main(argv: list = sys.argv[1:]):
+    if len(argv) > 0:
+        if "--version" in argv:
+            print(f"armory: {armory.version.__version__}")
+            print(f"JATIC-toolbox: {jatic_version}")
+            sys.exit(0)
+
+    print("Armory: Example Programmatic Entrypoint for Scenario Execution")
+
+
+    train_dataset = load_huggingface_dataset()
+
+
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
