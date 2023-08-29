b0eb7adf90238b93ea3a527904f0d058e3e33e5b
Honaker <christopher.honaker@twosixtech.com>
2023-08-29 09:34:02 -0400

    black command

--- examples/src/charmory_examples/xview_example.py
+++ examples/src/charmory_examples/xview_example.py
@@ -9,22 +9,17 @@ from jatic_toolbox import load_dataset as load_jatic_dataset
 import armory.baseline_models.pytorch.pokemon
 import armory.version
 
-dd
 
 def load_huggingface_dataset():
-    
-
     train_dataset = load_jatic_dataset(
         provider="huggingface",
         dataset_name="Honaker/xview_dataset",
         task="object-detection",
         split="train",
-       
     )
 
- 
-
     return train_dataset
+
 
 def main(argv: list = sys.argv[1:]):
     if len(argv) > 0:
@@ -35,11 +30,8 @@ def main(argv: list = sys.argv[1:]):
 
     print("Armory: Example Programmatic Entrypoint for Scenario Execution")
 
-
     train_dataset = load_huggingface_dataset()
 
-
-    
 
 if __name__ == "__main__":
     sys.exit(main())
