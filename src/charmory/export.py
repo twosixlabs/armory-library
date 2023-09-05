f11ca602326f92372416505e4c0edadec67d4bb4
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-09-05 13:27:26 -0400

    Export OD samples with boxes directly to MLFlow

--- src/charmory/export.py
+++ src/charmory/export.py
@@ -7,6 +7,8 @@ from typing import TYPE_CHECKING, Any, Dict, Optional, Union
 from PIL.Image import Image
 from mlflow.client import MlflowClient
 import numpy as np
+import torch
+from torchvision.utils import draw_bounding_boxes
 
 if TYPE_CHECKING:
     import matplotlib.figure
@@ -86,3 +88,35 @@ class MlflowExporter(Exporter):
         artifact_file: str,
     ):
         self.client.log_table(self.run_id, data, artifact_file)
+
+
+def draw_boxes_on_image(
+    image: np.ndarray,
+    ground_truth_boxes: Optional[np.ndarray] = None,
+    pred_boxes: Optional[np.ndarray] = None,
+) -> np.ndarray:
+    if image.shape[-1] in (1, 3, 6):  # Convert from (H, W, C) to (C, H, W)
+        image = image.transpose(2, 0, 1)
+
+    if image.dtype != np.uint8:  # Convert/scale to uint8
+        image = np.uint8(np.round(np.clip(image, 0.0, 1.0) * 255.0))
+
+    with_boxes = torch.as_tensor(image)
+
+    if ground_truth_boxes is not None:
+        with_boxes = draw_bounding_boxes(
+            image=with_boxes,
+            boxes=torch.as_tensor(ground_truth_boxes),
+            colors="red",
+            width=2,
+        )
+
+    if pred_boxes is not None:
+        with_boxes = draw_bounding_boxes(
+            image=with_boxes,
+            boxes=torch.as_tensor(pred_boxes),
+            colors="white",
+            width=2,
+        )
+
+    return with_boxes.numpy()
