846baeafc2e187b9299228866e8b684c62bd06ba
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-09-08 10:49:08 -0400

    Add data loader for JATIC OD dataset

--- src/charmory/data.py
+++ src/charmory/data.py
@@ -2,8 +2,13 @@
 
 # This could get merged with armory.data.datasets
 
+from typing import TYPE_CHECKING
+
 import numpy as np
 from torch.utils.data.dataloader import DataLoader
+
+if TYPE_CHECKING:
+    import jatic_toolbox.protocols
 
 from armory.data.datasets import ArmoryDataGenerator
 from charmory.track import track_init_params
@@ -58,11 +63,11 @@ class JaticVisionDataLoader(DataLoader):
 
     def __init__(
         self,
-        dataset,
+        dataset: "jatic_toolbox.protocols.VisionDataset",
         batch_size: int = 1,
         shuffle: bool = False,
-        image_key="image",
-        label_key="label",
+        image_key: str = "image",
+        label_key: str = "label",
         **kwargs,
     ):
         kwargs.pop("collate_fn", None)
@@ -71,6 +76,31 @@ class JaticVisionDataLoader(DataLoader):
             batch_size=batch_size,
             shuffle=shuffle,
             collate_fn=_collate_image_classification(image_key, label_key),
+            **kwargs,
+        )
+
+
+@track_init_params()
+class JaticObjectDetectionDataLoader(DataLoader):
+    """
+    Data loader for a JATIC object detection dataset.
+    """
+
+    def __init__(
+        self,
+        dataset: "jatic_toolbox.protocols.ObjectDetectionDataset",
+        batch_size: int = 1,
+        shuffle: bool = False,
+        image_key: str = "image",
+        objects_key: str = "objects",
+        **kwargs,
+    ):
+        kwargs.pop("collate_fn", None)
+        super().__init__(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            collate_fn=_collate_object_detection(image_key, objects_key),
             **kwargs,
         )
 
