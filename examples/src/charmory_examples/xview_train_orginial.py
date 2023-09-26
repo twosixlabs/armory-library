08959e0b1dab65613cdda7c71291e87c2238f8f5
Honaker <christopher.honaker@twosixtech.com>
2023-09-26 12:16:33 -0400

    add to train orginial

--- examples/src/charmory_examples/xview_train_orginial.py
+++ examples/src/charmory_examples/xview_train_orginial.py
@@ -80,8 +80,8 @@ LABEL_MAP = {
 
 def load_huggingface_dataset():
     train_data = datasets.load_dataset("Honaker/xview_dataset", split="train")
-    train_data = train_data.train_test_split(test_size=0.5, seed=1)
-    new_dataset = train_data['train'].train_test_split(test_size=0.5, seed=1)
+    #train_data = train_data.train_test_split(test_size=0.5, seed=1)
+    new_dataset = train_data.train_test_split(test_size=0.3, seed=1)
     train_dataset, test_dataset = new_dataset["train"], new_dataset["test"]
 
     #train_dataset, test_dataset = HuggingFaceObjectDetectionDataset(
@@ -230,8 +230,9 @@ def create_valid_loader(valid_dataset, num_workers=2):
     )
     return valid_loader
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor,FasterRCNN_MobileNet_V3_Large_320_FPN_Weights
+#model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(weights=FasterRCNN_MobileNet_V3_Large_320_FPN_Weights.DEFAULT)
 
 def create_model(num_classes):
     num_classes = num_classes   # 63 classes
@@ -241,8 +242,8 @@ def create_model(num_classes):
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
 
-train_dataset_1 = xview(train_dataset, 50, 50,  transforms=get_train_transform())
-test_dataset_1 = xview(test_dataset, 50, 50,  transforms=get_valid_transform())
+train_dataset_1 = xview(train_dataset, 3000, 3000,  transforms=get_train_transform())
+test_dataset_1 = xview(test_dataset, 3000, 3000,  transforms=get_valid_transform())
 train_loader = create_train_loader(train_dataset_1)
 valid_loader = create_valid_loader(test_dataset_1)
 print(f"Number of training samples: {len(train_dataset)}")
@@ -273,8 +274,8 @@ def train(train_data_loader, model):
         loss_dict = model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
         loss_value = losses.item()
-        #train_loss_list.append(loss_value)
-        #train_loss_hist.send(loss_value)
+        train_loss_list.append(loss_value)
+        train_loss_hist.send(loss_value)
         losses.backward()
         optimizer.step()
         train_itr += 1
@@ -311,14 +312,14 @@ def validate(valid_data_loader, model):
 
 
 import time
-NUM_EPOCHS= 1
+NUM_EPOCHS= 100
 # get the model parameters
 params = [p for p in model.parameters() if p.requires_grad]
 # define the optimizer
 optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
 # initialize the Averager class
-#train_loss_hist = Averager()
-#val_loss_hist = Averager()
+train_loss_hist = Averager()
+val_loss_hist = Averager()
 train_itr = 1
 val_itr = 1
 # train and validation loss lists to store loss values of all...
@@ -334,25 +335,26 @@ MODEL_NAME = 'model'
 for epoch in range(NUM_EPOCHS):
     print(f"\nEPOCH {epoch+1} of {NUM_EPOCHS}")
     # reset the training and validation loss histories for the current epoch
-    #train_loss_hist.reset()
-    #val_loss_hist.reset()
+    train_loss_hist.reset()
+    val_loss_hist.reset()
     # start timer and carry out training and validation
     start = time.time()
     train_loss = train(train_loader, model)
     val_loss = validate(valid_loader, model)
-    #print(f"Epoch #{epoch+1} train loss: {train_loss_hist.value:.3f}")   
-    #print(f"Epoch #{epoch+1} validation loss: {val_loss_hist.value:.3f}")   
+    print(f"Epoch #{epoch+1} train loss: {train_loss_hist.value:.3f}")   
+    print(f"Epoch #{epoch+1} validation loss: {val_loss_hist.value:.3f}")   
     end = time.time()
     print(f"Took {((end - start) / 60):.3f} minutes for epoch {epoch}")
     # save the best model till now if we have the least loss in the...
     # ... current epoch
-    save_best_model(
-        val_loss_hist.value, epoch, model, optimizer
-    )
+    #save_best_model(
+    #    val_loss_hist.value, epoch, model, optimizer
+    #)
     # save the current epoch model
-    save_model(epoch, model, optimizer)
+    #save_model(epoch, model, optimizer)
     # save loss plot
-    save_loss_plot(OUT_DIR, train_loss, val_loss)
+    #save_loss_plot(OUT_DIR, train_loss, val_loss)
     
     # sleep for 5 seconds after each epoch
     time.sleep(5)
+torch.save(model, 'fasterrcnn_mobilenet_v3')
