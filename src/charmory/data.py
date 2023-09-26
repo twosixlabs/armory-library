c97e1fa445c65962ebc2faa62725191a87d427c2
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-09-26 10:56:17 -0400

    Remove obsolete data loader class

--- src/charmory/data.py
+++ src/charmory/data.py
@@ -73,6 +73,7 @@ class JaticObjectDetectionDataset(MapSampleDataset):
         super().__init__(dataset, x_key="image", y_key="objects")
 
 
+@track_init_params
 class ArmoryDataLoader(DataLoader):
     """
     Customization of the PyTorch DataLoader to produce numpy arrays instead of
@@ -87,50 +88,3 @@ class ArmoryDataLoader(DataLoader):
     def _collate(batch):
         x, y = zip(*batch)
         return np.asarray(x), np.asarray(y)
-
-
-def _collate_image_classification(image_key, label_key):
-    """Create a collate function that works with image classification samples"""
-
-    def collate(batch):
-        x = np.asarray([sample[image_key] for sample in batch])
-        y = np.asarray([sample[label_key] for sample in batch])
-        return x, y
-
-    return collate
-
-
-def _collate_object_detection(image_key, objects_key):
-    """Create a collate function that works with object detection samples"""
-
-    def collate(batch):
-        x = np.asarray([sample[image_key] for sample in batch])
-        y = [sample[objects_key] for sample in batch]
-        return x, y
-
-    return collate
-
-
-@track_init_params()
-class JaticObjectDetectionDataLoader(DataLoader):
-    """
-    Data loader for a JATIC object detection dataset.
-    """
-
-    def __init__(
-        self,
-        dataset: "jatic_toolbox.protocols.ObjectDetectionDataset",
-        batch_size: int = 1,
-        shuffle: bool = False,
-        image_key: str = "image",
-        objects_key: str = "objects",
-        **kwargs,
-    ):
-        kwargs.pop("collate_fn", None)
-        super().__init__(
-            dataset=dataset,
-            batch_size=batch_size,
-            shuffle=shuffle,
-            collate_fn=_collate_object_detection(image_key, objects_key),
-            **kwargs,
-        )
