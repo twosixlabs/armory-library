7573e36f4601cd51997dfddd66fd9920a6e7d2d2
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-09-28 14:29:23 -0400

    Export target information to mlflow

--- src/charmory/tasks/image_classification.py
+++ src/charmory/tasks/image_classification.py
@@ -1,5 +1,6 @@
 """Image classification evaluation task"""
 
+import numpy as np
 import torch
 import torchmetrics.classification
 
@@ -27,15 +28,35 @@ class ImageClassificationTask(BaseEvaluationTask):
         self.perturbation = PerturbationNormMetric(ord=perturbation_ord)
 
     def export_batch(self, batch: BaseEvaluationTask.Batch):
-        self._export("x", batch.x, batch.i)
+        self._export_image("x", batch.x, batch.i)
         if batch.x_adv is not None:
-            self._export("x_adv", batch.x_adv, batch.i)
+            self._export_image("x_adv", batch.x_adv, batch.i)
+        self._export_targets(batch)
 
-    def _export(self, name, batch_data, batch_idx):
+    def _export_image(self, name, batch_data, batch_idx):
         batch_size = batch_data.shape[0]
         for sample_idx in range(batch_size):
             filename = f"batch_{batch_idx}_ex_{sample_idx}_{name}.png"
             self.exporter.log_image(batch_data[sample_idx], filename)
+
+    @staticmethod
+    def _serialize(numpy_array_or_list):
+        if isinstance(numpy_array_or_list, np.ndarray):
+            return numpy_array_or_list.tolist()
+        return numpy_array_or_list
+
+    def _export_targets(self, batch):
+        targets = dict(
+            i=batch.i,
+            y=self._serialize(batch.y),
+            y_pred=self._serialize(batch.y_pred),
+            y_target=self._serialize(batch.y_target),
+            y_pred_adv=self._serialize(batch.y_pred_adv),
+        )
+        self.exporter.log_dict(
+            dictionary=targets,
+            artifact_file=f"batch_{batch.i}_targets.txt",
+        )
 
     def run_benign(self, batch: BaseEvaluationTask.Batch):
         super().run_benign(batch)
