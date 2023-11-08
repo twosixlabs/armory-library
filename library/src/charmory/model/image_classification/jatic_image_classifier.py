8190b6e85cceace47709a2945d7a344a63caf97f
Kyle Treubig <kyle.treubig@twosixtech.com>
2023-11-08 13:31:13 -0500

    Update docstrings for charmory.model

--- library/src/charmory/model/image_classification/jatic_image_classifier.py
+++ library/src/charmory/model/image_classification/jatic_image_classifier.py
@@ -8,13 +8,34 @@ from charmory.model.base import ArmoryModel, ModelInputAdapter
 
 
 class JaticImageClassificationModel(ArmoryModel):
-    """Model wrapper with pre-applied output adapter for JATIC image classification models"""
+    """
+    Model wrapper with pre-applied output adapter for JATIC image classification
+    models.
+
+    Example::
+        import jatic_toolbox
+        from charmory.model.image_classification import JaticImageClassificationModel
+
+        model = jatic_toolbox.load_model(
+            provider=PROVIDER,
+            model_name=MODEL_NAME,
+            task="image-classification",
+        )
+        model = JaticImageClassificationModel(model)
+    """
 
     def __init__(
         self,
         model: "jatic_toolbox.protocols.ImageClassifier",
         preadapter: Optional[ModelInputAdapter] = None,
     ):
+        """
+        Initializes the model wrapper.
+
+        Args:
+            model: model being wrapped
+            preadapter: Optional, model input adapter
+        """
         super().__init__(model, preadapter=preadapter, postadapter=self._adapt)
 
     def _adapt(self, output):
