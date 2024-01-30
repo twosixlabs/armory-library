19136fde590984b2fdc465db4ff16c1a3f65f8b7
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-01-30 15:54:05 -0500

    Split and save dataset in huggingface format

--- examples/src/armory/examples/image_classification/icon_classifier/icon645.py
+++ examples/src/armory/examples/image_classification/icon_classifier/icon645.py
@@ -1,8 +1,10 @@
 """Icon645 dataset from IconQA"""
 import os
+from pprint import pprint
 import shutil
 import zipfile
 
+import datasets
 import requests
 from tqdm.auto import tqdm
 from tqdm.utils import CallbackIOWrapper
@@ -10,10 +12,11 @@ from tqdm.utils import CallbackIOWrapper
 from armory.evaluation import SysConfig
 
 DOWNLOAD_URL = "https://iconqa2021.s3.us-west-1.amazonaws.com/icon645.zip"
+IMAGES_SUBDIR = "colored_icons_final"
 
 
 class CachePaths:
-    def __init__(self, sysconfig: SysConfig):
+    def __init__(self, sysconfig: SysConfig = SysConfig()):
         self.base_path = sysconfig.dataset_cache / "icon645"
         self.zip_file = self.base_path / "icon645.zip"
         self.images_path = self.base_path / "images"
@@ -70,11 +73,59 @@ def extract_zip(paths: CachePaths):
                         )
 
 
-def prepare_dataset(paths: CachePaths):
+def rename_split_folder(paths: CachePaths):
+    train_path = paths.images_path / "train"
+    if train_path.exists():
+        print(f"{train_path} already exists")
+        return
+
+    shutil.move(paths.images_path / IMAGES_SUBDIR, train_path)
+
+
+def load_images_dataset(paths: CachePaths) -> datasets.Dataset:
+    print(f"Loading {paths.images_path}...")
+    dataset = datasets.load_dataset(
+        "imagefolder", data_dir=str(paths.images_path), split="train"
+    )
+    assert isinstance(dataset, datasets.Dataset)
+    return dataset
+
+
+def split_dataset(dataset: datasets.Dataset) -> datasets.DatasetDict:
+    print("Creating train/test/validation splits...")
+    dsdict = dataset.train_test_split(test_size=0.2, stratify_by_column="label")
+    val_train = dsdict["train"].train_test_split(
+        test_size=0.25, stratify_by_column="label"
+    )
+    dsdict["train"] = val_train["train"]
+    dsdict["validation"] = val_train["test"]
+    return dsdict
+
+
+def prepare_dataset(paths: CachePaths = CachePaths()):
+    if paths.dataset_path.exists():
+        print(f"{paths.dataset_path} already exists")
+        return
+
     paths.base_path.mkdir(parents=True, exist_ok=True)
     download_zip(paths)
     extract_zip(paths)
+    rename_split_folder(paths)
+    img_ds = load_images_dataset(paths)
+    ds_splits = split_dataset(img_ds)
+    ds_splits.save_to_disk(paths.dataset_path)
+    print("Done creating dataset cache")
+
+
+def load_dataset(paths: CachePaths = CachePaths()):
+    if not paths.dataset_path.exists():
+        raise RuntimeError(
+            "Dataset has not been cached, run `prepare_dataset` to download and cache the dataset first"
+        )
+    return datasets.load_from_disk(str(paths.dataset_path))
 
 
 if __name__ == "__main__":
-    prepare_dataset(CachePaths(SysConfig()))
+    prepare_dataset()
+    ds = load_dataset()
+    pprint(ds)
