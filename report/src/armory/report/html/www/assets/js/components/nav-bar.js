f674d3534c87ccfb29a619a1283fda4b9d3b4591
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-02-09 12:42:17 -0500

    Remove custom route utils

--- report/src/armory/report/html/www/assets/js/components/nav-bar.js
+++ report/src/armory/report/html/www/assets/js/components/nav-bar.js
@@ -1,47 +1,16 @@
-import { computed } from 'vue';
-import { useRoute } from "../stores/route.js";
-
-const NavLink = {
-    props: {
-        route: String,
-    },
-    setup(props) {
-        const routes = useRoute();
-        const onClick = () => routes.setRoute(props.route);
-
-        const classes = computed({
-            get() {
-                return {
-                    'bg-twosix-green': props.route == routes.route,
-                };
-            },
-        });
-
-        return { classes, onClick };
-    },
-    template: `
-        <a
-            @click.prevent="onClick"
-            :class="classes"
-            class="hover:bg-twosix-grey hover:cursor-pointer my-auto p-2 rounded text-medium uppercase"
-        >
-            <slot></slot>
-        </a>
-    `,
-};
+import { RouterLink } from 'vue-router';
 
 export default {
     components: {
-        NavLink,
+        RouterLink,
     },
     template: `
         <div class="flex flex-row">
             <h1 class="font-medium text-xl mr-4 my-auto text-twosix-black uppercase">
-                Evaluation Report
+                <router-link to="/">
+                    Evaluation Report
+                </router-link>
             </h1>
-            <nav-link route="summary">Summary</nav-link>
-            <nav-link route="parameters">Parameters</nav-link>
-            <nav-link route="metrics">Metrics</nav-link>
         </div>
         <div>
             <img src="./assets/img/armory.png" alt="Armory" class="h-12" />
