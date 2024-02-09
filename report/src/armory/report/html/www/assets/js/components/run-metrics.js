34fd4e919a267d9eb07ac7f07eab36c84d6896a7
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-02-09 14:51:21 -0500

    Format metrics to user-specified precision

--- report/src/armory/report/html/www/assets/js/components/run-metrics.js
+++ report/src/armory/report/html/www/assets/js/components/run-metrics.js
@@ -1,3 +1,5 @@
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import {
     Table,
     TableBody,
@@ -41,10 +43,35 @@ export default {
         metrics: Object,
     },
     setup(props) {
+        const route = useRoute();
+        const router = useRouter();
+
+        const precision = computed({
+            get() {
+                return route.query.precision || 3;
+            },
+            set(precision) {
+                router.push({ query: { precision } });
+            },
+        });
+
         const [metricsByChain, allMetrics] = reorganizeMetrics(props.metrics);
-        return { allMetrics, metricsByChain };
+
+        return { allMetrics, metricsByChain, precision };
     },
     template: `
+        <div class="my-2">
+            <span class="mr-2">
+                Precision
+            </span>
+            <input
+                v-model="precision"
+                class="appearance-none border border-zinc-300 focus:border-zinc-400 focus:outline-none leading-6 pl-3 pr-2 py-1.5 rounded-md w-20"
+                min="0"
+                max="9"
+                type="number"
+            />
+        </div>
         <Table>
             <TableHead>
                 <tr>
@@ -60,7 +87,7 @@ export default {
                         {{ entry[0] }}
                     </TableRowHeader>
                     <TableCell v-for="metric in allMetrics" :key="metric">
-                        {{ entry[1][metric] }}
+                        {{ entry[1][metric].toFixed(precision) }}
                     </TableCell>
                 </TableRow>
             </TableBody>
