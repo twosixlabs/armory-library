9aa7d3a943de9e3ecf262836558ce281d38a1db9
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-02-21 17:30:12 -0500

    Create version of food101 with no armory, tracking, or exporting

---
+++ examples/src/armory/examples/image_classification/food101_no_armory.py
@@ -0,0 +1,200 @@
+"""
+Example evaluation of food-101 image classification against projected
+gradient descent (PGD) adversarial perturbation
+"""
+
+import functools
+from pprint import pprint
+
+import art.attacks.evasion
+import art.defences.preprocessor
+import art.estimators.classification.hugging_face
+import datasets
+import torch
+import torch.nn
+import torch.utils.data.dataloader
+import torchmetrics
+import torchmetrics.classification
+import torchvision.transforms
+import transformers
+
+# Model
+
+
+hf_model = transformers.AutoModelForImageClassification.from_pretrained(
+    pretrained_model_name_or_path="nateraw/food"
+)
+
+art_classifier = (
+    art.estimators.classification.hugging_face.HuggingFaceClassifierPyTorch(
+        hf_model,
+        loss=torch.nn.CrossEntropyLoss(),
+        optimizer=torch.optim.Adam(hf_model.parameters(), lr=0.003),
+        input_shape=(3, 224, 224),
+        channels_first=True,
+        nb_classes=101,
+        clip_values=(-1.0, 1.0),
+    )
+)
+
+
+# Dataset
+
+
+def transform(processor, sample):
+    """Use the HF image processor and convert from BW To RGB"""
+    sample["image"] = processor([img.convert("RGB") for img in sample["image"]])[
+        "pixel_values"
+    ]
+    return sample
+
+
+hf_dataset = datasets.load_dataset("food101", split="validation")
+assert isinstance(hf_dataset, datasets.Dataset)
+
+labels = hf_dataset.features["label"].names
+
+hf_processor = transformers.AutoImageProcessor.from_pretrained("nateraw/food")
+hf_dataset.set_transform(functools.partial(transform, hf_processor))
+
+dataloader = torch.utils.data.dataloader.DataLoader(
+    hf_dataset,
+    batch_size=16,
+)
+
+
+MEAN = (0.5, 0.5, 0.5)
+STD = (0.5, 0.5, 0.5)
+
+normalize = torchvision.transforms.Normalize(MEAN, STD)
+
+
+def unnormalize(data):
+    unnormalized = data.copy()
+    for image in unnormalized:
+        for c, m, s in zip(image, MEAN, STD):
+            c *= s
+            c += m
+    return unnormalized
+
+
+# Attack
+
+
+pgd = art.attacks.evasion.ProjectedGradientDescent(
+    art_classifier,
+    batch_size=1,
+    eps=0.003,
+    eps_step=0.0007,
+    max_iter=20,
+    num_random_init=1,
+    random_eps=False,
+    targeted=False,
+    verbose=False,
+)
+
+
+# Defense
+
+
+jpeg_compression = art.defences.preprocessor.JpegCompression(
+    clip_values=(0.0, 1.0),
+    quality=50,
+    channels_first=True,
+)
+
+
+# Metrics
+
+
+class PerturbationNormMetric(torchmetrics.Metric):
+    def __init__(self):
+        super().__init__()
+        self.distance: torch.Tensor
+        self.total: torch.Tensor
+        self.add_state(
+            "distance",
+            default=torch.tensor(0, dtype=torch.float32),
+            dist_reduce_fx="sum",
+        )
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, natural, perturbed):
+        self.distance += torch.norm((natural - perturbed).flatten(), p=torch.inf)
+        self.total += torch.tensor(1)
+
+    def compute(self):
+        return self.distance.float() / self.total
+
+
+benign_accuracy = torchmetrics.classification.Accuracy(
+    task="multiclass", num_classes=101
+)
+
+attacked_accuracy = torchmetrics.classification.Accuracy(
+    task="multiclass", num_classes=101
+)
+attacked_linf_norm = PerturbationNormMetric()
+
+defended_accuracy = torchmetrics.classification.Accuracy(
+    task="multiclass", num_classes=101
+)
+defended_linf_norm = PerturbationNormMetric()
+
+
+# Evaluation
+
+
+num_batches = 0
+for batch in dataloader:
+    label = batch["label"]
+    image = batch["image"].cpu().numpy()
+
+    # benign
+    benign_preds = art_classifier.predict(image)
+    benign_accuracy.update(
+        torch.as_tensor(benign_preds),
+        label,
+    )
+
+    # attacked
+    attacked_image = pgd.generate(
+        x=image,
+        y=label.cpu().numpy(),
+    )
+    attacked_preds = art_classifier.predict(attacked_image)
+    attacked_accuracy.update(
+        torch.as_tensor(attacked_preds),
+        label,
+    )
+    attacked_linf_norm.update(image, torch.as_tensor(attacked_image))
+
+    # defended
+    # have to de-normalize the image for JPEG compression, then re-normalize it
+    # before invoking the model
+    attacked_image_unnormed = unnormalize(attacked_image)
+    defended_image_unnormed, _ = jpeg_compression(attacked_image_unnormed)
+    defended_image = normalize(torch.as_tensor(defended_image_unnormed)).cpu().numpy()
+    defended_preds = art_classifier.predict(defended_image)
+    defended_accuracy.update(
+        torch.as_tensor(defended_preds),
+        label,
+    )
+    defended_linf_norm.update(image, torch.as_tensor(defended_image))
+
+    num_batches += 1
+    if num_batches >= 5:
+        break
+
+
+pprint(
+    {
+        "metrics": {
+            "benign/accuracy": benign_accuracy.compute(),
+            "attacked/accuracy": attacked_accuracy.compute(),
+            "attacked/linf_norm": attacked_linf_norm.compute(),
+            "defended/accuracy": defended_accuracy.compute(),
+            "defended/linf_norm": defended_linf_norm.compute(),
+        }
+    }
+)
