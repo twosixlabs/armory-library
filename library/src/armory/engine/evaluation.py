520437d5ae7b9123d365b32403c69386a7fbb553
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-04-24 10:27:54 -0400

    Collect system metrics correctly in nested runs

--- library/src/armory/engine/evaluation.py
+++ library/src/armory/engine/evaluation.py
@@ -6,7 +6,6 @@ from typing import Any, Dict, Mapping, Optional, TypedDict
 import lightning.pytorch as pl
 import lightning.pytorch.loggers as pl_loggers
 from lightning.pytorch.utilities import rank_zero_only
-import mlflow
 from torch import Tensor
 
 from armory.engine.evaluation_module import EvaluationModule
@@ -43,7 +42,6 @@ class EvaluationEngine:
         evaluation: Evaluation,
         profiler: Optional[Profiler] = None,
         sysconfig: Optional[SysConfig] = None,
-        # run_id: Optional[str] = None,
         **kwargs,
     ):
         """
@@ -51,6 +49,9 @@ class EvaluationEngine:
 
         Args:
             evaluation: Configuration for the evaluation
+            profiler: Optional, profiler to collect computational metrics. By
+                default, no computational metrics will be collected.
+            sysconfig: Optional, custom system configuration
             run_id: Optional, MLflow run ID to which to record evaluation results
             **kwargs: All other keyword arguments will be forwarded to the
                 `lightning.pytorch.Trainer` class.
@@ -66,20 +67,6 @@ class EvaluationEngine:
         """Log tracked params with MLFlow"""
         params["Armory.version"] = armory.version.__version__
         logger.log_hyperparams(params)
-
-    @rank_zero_only
-    def _create_nested_run_ids(
-        self, logger: pl_loggers.MLFlowLogger
-    ) -> Optional[Dict[str, str]]:
-        """Create nested MLFlow run IDs for each perturbation chain"""
-        run_ids = {}
-        with mlflow.start_run(run_id=logger.run_id):
-            for chain_name in self.evaluation.chains:
-                with mlflow.start_run(
-                    experiment_id=logger.experiment_id, run_name=chain_name, nested=True
-                ) as run:
-                    run_ids[chain_name] = run.info.run_id
-        return run_ids
 
     def _track_system_metrics(self, run_id: Optional[str]):
         """Track system metrics with MLFlow"""
@@ -105,31 +92,46 @@ class EvaluationEngine:
         self.run_id = logger.run_id
         self._log_params(logger, get_current_params())
 
-        results: Dict[str, EvaluationResults] = {}
-        chain_run_ids = self._create_nested_run_ids(logger) or {}
-        for chain_name, chain in self.evaluation.chains.items():
-            chain_run_id = chain_run_ids.get(chain_name, None)
-            results[chain_name] = self._evaluate_chain(chain_run_id, chain_name, chain)
+        try:
+            results: Dict[str, EvaluationResults] = {}
+            with self._track_system_metrics(self.run_id):
+                for chain_name, chain in self.evaluation.chains.items():
+                    results[chain_name] = self._evaluate_chain(
+                        logger.run_id, chain_name, chain
+                    )
+            logger.finalize()
+
+        except BaseException as err:
+            logger.finalize("failed")
+            raise RuntimeError(
+                f"Error performing evaluation of chain {chain_name}"
+            ) from err
+
         return results
 
     def _evaluate_chain(
-        self, chain_run_id: Optional[str], chain_name: str, chain: Chain
+        self,
+        parent_run_id: Optional[str],
+        chain_name: str,
+        chain: Chain,
     ) -> EvaluationResults:
         assert chain.dataset
 
         logger = pl_loggers.MLFlowLogger(
-            run_id=chain_run_id,
+            experiment_name=self.evaluation.name,
+            run_name=chain_name,
             tracking_uri=init_tracking_uri(self.sysconfig.armory_home),
+            tags={"mlflow.parentRunId": parent_run_id},
         )
         self._log_params(logger, chain.get_tracked_params())
 
+        module = EvaluationModule(chain, self.profiler)
+        trainer = pl.Trainer(
+            inference_mode=False,
+            logger=logger,
+            **self.trainer_kwargs,
+        )
         with self._track_system_metrics(logger.run_id):
-            module = EvaluationModule(chain, self.profiler)
-            trainer = pl.Trainer(
-                inference_mode=False,
-                logger=logger,
-                **self.trainer_kwargs,
-            )
             trainer.test(module, dataloaders=chain.dataset.dataloader)
 
         return EvaluationResults(
