b55a5ca5e889b9f34442142c7dc273dbcf35d219
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-05-24 10:00:51 -0400

    Plot tables using dash

--- library/src/armory/results/results.py
+++ library/src/armory/results/results.py
@@ -2,12 +2,23 @@
 
 from collections import UserDict
 from functools import cached_property
+import os
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
 if TYPE_CHECKING:
     import mlflow.client
     import mlflow.entities
     import rich.console
+
+
+_NEXT_PORT = int(os.getenv("PORT", "8050"))
+
+
+def _get_next_port() -> str:
+    global _NEXT_PORT
+    port = _NEXT_PORT
+    _NEXT_PORT += 1
+    return str(port)
 
 
 class EvaluationResults:
@@ -39,23 +50,23 @@ class EvaluationResults:
                 for k in info.__dir__()
                 if k[0] != "_" and type(getattr(info, k, "")).__name__ != "method"
             },
-            label="Details",
+            title="Details",
         )
 
     @cached_property
     def params(self) -> "RunDataDict":
         """Run parameters"""
-        return RunDataDict(data=self._run.data.params, label="Parameters")
+        return RunDataDict(data=self._run.data.params, title="Parameters")
 
     @cached_property
     def tags(self) -> "RunDataDict":
         """Run tags"""
-        return RunDataDict(data=self._run.data.tags, label="Tags")
+        return RunDataDict(data=self._run.data.tags, title="Tags")
 
     @cached_property
     def metrics(self) -> "RunDataDict":
         """Run metrics"""
-        return RunDataDict(data=self._run.data.metrics, label="Metrics")
+        return RunDataDict(data=self._run.data.metrics, title="Metrics")
 
     @cached_property
     def children(self) -> Dict[str, "EvaluationResults"]:
@@ -73,23 +84,30 @@ class EvaluationResults:
 class RunDataDict(UserDict[str, Any]):
     """Dictionary of run data that can be printed as a table"""
 
-    def __init__(self, data: Dict[str, Any], label: str):
+    def __init__(
+        self,
+        data: Dict[str, Any],
+        title: str,
+        key_label: str = "key",
+        value_label: str = "value",
+    ):
         """
         Initializes the data dictionary.
 
         Args:
             data: Dictionary contents
-            label: Label for the data
+            title: Title for the dictionary when printed as a table
+            key_label: Optional, label for the key column when printed as a table
+            value_label: Optional, label for the value column when printed as a table
         """
         super().__init__(data)
-        self.label = label
+        self.title = title
+        self.key_label = key_label
+        self.value_label = value_label
 
     def table(
         self,
         console: Optional["rich.console.Console"] = None,
-        key_label: str = "key",
-        title: Optional[str] = None,
-        value_label: str = "value",
         **kwargs,
     ) -> None:
         """
@@ -98,17 +116,13 @@ class RunDataDict(UserDict[str, Any]):
         Args:
             console: Optional, rich console to use for printing. Defaults to the
                 standard rich console.
-            key_label: Optional, label for the key column
-            title: Optional, title for the table. Defaults to the dictionary
-                label.
-            value_label: Optional, label for the value column
             **kwargs: Keyword arguments forwarded to the rich table constructor
         """
         from rich.table import Table
 
-        table = Table(title=title or self.label, **kwargs)
-        table.add_column(key_label, style="cyan", no_wrap=True)
-        table.add_column(value_label, style="magenta")
+        table = Table(title=self.title, **kwargs)
+        table.add_column(self.key_label, style="cyan", no_wrap=True)
+        table.add_column(self.value_label, style="magenta")
 
         for key, value in sorted(self.items()):
             table.add_row(key, str(value))
@@ -119,3 +133,55 @@ class RunDataDict(UserDict[str, Any]):
             console = Console()
 
         console.print(table)
+
+    def plot(self, debug: bool = False, height: int = 400, port: Optional[int] = None):
+        import dash
+
+        data = [{"key": key, "value": value} for key, value in sorted(self.items())]
+
+        app = dash.Dash()
+        app.layout = dash.html.Div(
+            children=[
+                dash.html.H1(children=self.title, style={"textAlign": "center"}),
+                dash.dash_table.DataTable(
+                    data=data,
+                    columns=[
+                        {"id": "key", "name": self.key_label},
+                        {"id": "value", "name": self.value_label},
+                    ],
+                    style_header={
+                        "fontWeight": "bold",
+                        "backgroundColor": "rgb(230, 230, 230)",
+                        "textAlign": "center",
+                    },
+                    style_cell={
+                        "overflow": "hidden",
+                        "textAlign": "left",
+                        "textOverflow": "ellipsis",
+                    },
+                    style_cell_conditional=[
+                        {"if": {"column_id": "key"}, "width": "20%"},
+                        {"if": {"column_id": "value"}, "maxWidth": 0},
+                    ],
+                    style_data_conditional=[
+                        {
+                            "if": {"row_index": "odd"},
+                            "backgroundColor": "rgb(240, 240, 240)",
+                        },
+                    ],
+                    tooltip_data=[{"value": str(e["value"])} for e in data],
+                    tooltip_delay=1000,
+                    tooltip_duration=None,
+                ),
+            ],
+            style={"background": "white"},
+        )
+
+        app.run(
+            debug=debug,
+            jupyter_height=height,
+            port=str(port) if port is not None else _get_next_port(),
+        )
+
+    def _ipython_display_(self):
+        self.plot()
