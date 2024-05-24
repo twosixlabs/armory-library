356c46c5c22b60a41b30f2a9b0ae59e308eb4919
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-05-24 13:53:28 -0400

    Create metrics-specific class that adds precision formatting

--- library/src/armory/results/results.py
+++ library/src/armory/results/results.py
@@ -3,7 +3,7 @@
 from collections import UserDict
 from functools import cached_property
 import os
-from typing import TYPE_CHECKING, Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional
 
 if TYPE_CHECKING:
     import mlflow.client
@@ -64,9 +64,9 @@ class EvaluationResults:
         return RunDataDict(data=self._run.data.tags, title="Tags")
 
     @cached_property
-    def metrics(self) -> "RunDataDict":
+    def metrics(self) -> "RunMetricsDict":
         """Run metrics"""
-        return RunDataDict(data=self._run.data.metrics, title="Metrics")
+        return RunMetricsDict(data=self._run.data.metrics, title="Metrics")
 
     @cached_property
     def children(self) -> Dict[str, "EvaluationResults"]:
@@ -108,6 +108,7 @@ class RunDataDict(UserDict[str, Any]):
     def table(
         self,
         console: Optional["rich.console.Console"] = None,
+        format: Callable[[Any], str] = str,
         **kwargs,
     ) -> None:
         """
@@ -116,6 +117,8 @@ class RunDataDict(UserDict[str, Any]):
         Args:
             console: Optional, rich console to use for printing. Defaults to the
                 standard rich console.
+            format: Optional, function to format values for printing. Defaults to
+                the built-in str function.
             **kwargs: Keyword arguments forwarded to the rich table constructor
         """
         from rich.table import Table
@@ -125,7 +128,7 @@ class RunDataDict(UserDict[str, Any]):
         table.add_column(self.value_label, style="magenta")
 
         for key, value in sorted(self.items()):
-            table.add_row(key, str(value))
+            table.add_row(key, format(value))
 
         if console is None:
             from rich.console import Console
@@ -138,12 +141,27 @@ class RunDataDict(UserDict[str, Any]):
         self,
         dark: bool = False,
         debug: bool = False,
+        format: Callable[[Any], str] = str,
         height: int = 400,
         port: Optional[int] = None,
-    ):
+    ) -> None:
+        """
+        Displays the contents of the dictionary in an HTML table.
+
+        Args:
+            dark: Optional, use a dark theme if True
+            debug: Optional, enable debug output from Dash
+            format: Optional, function to format values for printing. Defaults to
+                the built-in str function.
+            height: Optional, height of the table in pixels
+            port: Optional, port to use for the Dash server. Defaults to the next
+                available port.
+        """
         import dash
 
-        data = [{"key": key, "value": value} for key, value in sorted(self.items())]
+        data = [
+            {"key": key, "value": format(value)} for key, value in sorted(self.items())
+        ]
 
         app = dash.Dash()
         app.layout = dash.html.Div(
@@ -208,3 +226,82 @@ class RunDataDict(UserDict[str, Any]):
 
     def _ipython_display_(self):
         self.plot()
+
+
+class RunMetricsDict(RunDataDict):
+
+    def __init__(
+        self,
+        data: Dict[str, Any],
+        title: str,
+        key_label: str = "metric",
+        value_label: str = "value",
+    ):
+        """
+        Initializes the metrics dictionary.
+
+        Args:
+            data: Dictionary contents
+            title: Title for the dictionary when printed as a table
+            key_label: Optional, label for the key column when printed as a table
+            value_label: Optional, label for the value column when printed as a table
+        """
+        super().__init__(
+            data=data, title=title, key_label=key_label, value_label=value_label
+        )
+
+    def table(
+        self,
+        console: Optional["rich.console.Console"] = None,
+        format: Optional[Callable[[Any], str]] = None,
+        precision: int = 3,
+        **kwargs,
+    ) -> None:
+        """
+        Prints the contents of the dictionary in a rich table.
+
+        Args:
+            console: Optional, rich console to use for printing. Defaults to the
+                standard rich console.
+            format: Optional, function to format values for printing. Defaults to
+                a fixed-precision floating point formatter.
+            precision: Optional, number of decimal places to display for floating
+                point values
+            **kwargs: Keyword arguments forwarded to the rich table constructor
+        """
+        return super().table(
+            console=console,
+            format=format or (lambda v: f"{v:.{precision}f}"),
+            **kwargs,
+        )
+
+    def plot(
+        self,
+        dark: bool = False,
+        debug: bool = False,
+        format: Optional[Callable[[Any], str]] = None,
+        height: int = 400,
+        precision: int = 3,
+        port: Optional[int] = None,
+    ) -> None:
+        """
+        Displays the contents of the dictionary in an HTML table.
+
+        Args:
+            dark: Optional, use a dark theme if True
+            debug: Optional, enable debug output from Dash
+            format: Optional, function to format values for printing. Defaults to
+                a fixed-precision floating point formatter.
+            height: Optional, height of the table in pixels
+            precision: Optional, number of decimal places to display for floating
+                point values
+            port: Optional, port to use for the Dash server. Defaults to the next
+                available port.
+        """
+        return super().plot(
+            dark=dark,
+            debug=debug,
+            format=format or (lambda v: f"{v:.{precision}f}"),
+            height=height,
+            port=port,
+        )
