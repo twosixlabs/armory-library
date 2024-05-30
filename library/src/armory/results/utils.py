ee84741cbaff1321ddc294e3e62038a2ee211c7c
Kyle Treubig <kyle.treubig@twosixtech.com>
2024-05-30 15:09:28 -0400

    Remove commented util function

--- library/src/armory/results/utils.py
+++ library/src/armory/results/utils.py
@@ -20,24 +20,3 @@ def get_next_dash_port() -> str:
     port = _NEXT_DASH_PORT
     _NEXT_DASH_PORT += 1
     return str(port)
-
-
-# def for_experiment(
-#     name: Optional[str] = None,
-#     experiment_id: Optional[str] = None,
-#     max_results: int = 100,
-# ) -> Sequence[EvaluationResults]:
-#     client = _get_mlflow_client()
-#     if name is not None and experiment_id is None:
-#         experiments = client.search_experiments(filter_string=f"name = '{name}'")
-#         if len(experiments) == 1:
-#             experiment_id = experiments[0].experiment_id
-#         else:
-#             raise RuntimeError(f"No experiment found with name '{name}'")
-#     if experiment_id is None:
-#         raise ValueError("No experiment ID or name provided")
-
-#     runs = client.search_runs(experiment_ids=[experiment_id], max_results=max_results)
-#     if len(runs) == 0:
-#         raise RuntimeError("No runs found for experiment")
-#     return [EvaluationResults(client, run) for run in runs]
