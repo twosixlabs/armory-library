6cacbd5924e76672d13d3e3d987554bb554e1c75
Jay Soni <jsoni@px.ftw>
2024-10-01 14:33:36 -0400

    170-api-docs: lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../library/src/armory'))
+
+sys.path.insert(0, os.path.abspath("../../library/src/armory"))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -10,23 +11,22 @@ sys.path.insert(0, os.path.abspath('../../library/src/armory'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'armory-library'
-copyright = '2024, Two Six Technologies'
-author = 'Two Six Technologies'
-release = '1.0.0'
+project = "armory-library"
+copyright = "2024, Two Six Technologies"
+author = "Two Six Technologies"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
